f5f43b134531c21f08e3aedab94ce3e9cacd628d
Rafael Luque <rafael.luque@osoco.es>
2019-01-15 12:31:47 +0000

    Historical changes pending to be pushed

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -7,23 +7,40 @@ import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
 import webpack from "webpack";
 import webpackConfig from "./webpack.conf";
+import svgstore from "gulp-svgstore";
+import svgmin from "gulp-svgmin";
+import inject from "gulp-inject";
+import replace from "gulp-replace";
+import cssnano from "cssnano";
 
 const browserSync = BrowserSync.create();
-const hugoBin = "hugo";
-const defaultArgs = ["-d", "../dist", "-s", "site", "-v"];
+const hugoBin = `./bin/hugo.${process.platform === "win32" ? "exe" : process.platform}`;
+const defaultArgs = ["-d", "../dist", "-s", "site"];
+
+if (process.env.DEBUG) {
+  defaultArgs.unshift("--debug")
+}
 
 gulp.task("hugo", (cb) => buildSite(cb));
 gulp.task("hugo-preview", (cb) => buildSite(cb, ["--buildDrafts", "--buildFuture"]));
-
-gulp.task("build", ["css", "js", "hugo"]);
-gulp.task("build-preview", ["css", "js", "hugo-preview"]);
+gulp.task("build", ["css", "js", "cms-assets", "hugo"]);
+gulp.task("build-preview", ["css", "js", "cms-assets", "hugo-preview"]);
 
 gulp.task("css", () => (
   gulp.src("./src/css/*.css")
-    .pipe(postcss([cssImport({from: "./src/css/main.css"}), cssnext()]))
+    .pipe(postcss([
+      cssImport({from: "./src/css/main.css"}),
+      cssnext(),
+      cssnano(),
+    ]))
     .pipe(gulp.dest("./dist/css"))
     .pipe(browserSync.stream())
 ));
+
+gulp.task("cms-assets", () => (
+  gulp.src("./node_modules/netlify-cms/dist/*.{woff,eot,woff2,ttf,svg,png}")
+    .pipe(gulp.dest("./dist/css"))
+))
 
 gulp.task("js", (cb) => {
   const myConfig = Object.assign({}, webpackConfig);
@@ -39,7 +56,23 @@ gulp.task("js", (cb) => {
   });
 });
 
-gulp.task("server", ["hugo", "css", "js"], () => {
+gulp.task("svg", () => {
+  const svgs = gulp
+    .src("site/static/img/icons-*.svg")
+    .pipe(svgmin())
+    .pipe(svgstore({inlineSvg: true}));
+
+  function fileContents(filePath, file) {
+    return file.contents.toString();
+  }
+
+  return gulp
+    .src("site/layouts/partials/svg.html")
+    .pipe(inject(svgs, {transform: fileContents}))
+    .pipe(gulp.dest("site/layouts/partials/"));
+});
+
+gulp.task("server", ["hugo", "css", "cms-assets", "js", "svg"], () => {
   browserSync.init({
     server: {
       baseDir: "./dist"
@@ -47,6 +80,7 @@ gulp.task("server", ["hugo", "css", "js"], () => {
   });
   gulp.watch("./src/js/**/*.js", ["js"]);
   gulp.watch("./src/css/**/*.css", ["css"]);
+  gulp.watch("./site/static/img/icons-*.svg", ["svg"]);
   gulp.watch("./site/**/*", ["hugo"]);
 });
 
@@ -55,7 +89,7 @@ function buildSite(cb, options) {
 
   return cp.spawn(hugoBin, args, {stdio: "inherit"}).on("close", (code) => {
     if (code === 0) {
-      browserSync.reload();
+      browserSync.reload("notify:false");
       cb();
     } else {
       browserSync.notify("Hugo build failed :(");
@@ -63,3 +97,5 @@ function buildSite(cb, options) {
     }
   });
 }
+
+
