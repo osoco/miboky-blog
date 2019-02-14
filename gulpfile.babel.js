5f44703eb25aace197ab7958c5e1e140ae0a4bd4
Rafael Luque <rafael.luque@osoco.es>
2019-02-14 08:44:18 +0000

    Remove gulp-sass

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,7 +2,6 @@ import gulp from "gulp";
 import cp from "child_process";
 import gutil from "gulp-util";
 import postcss from "gulp-postcss";
-import sass from "gulp-sass";
 import cssImport from "postcss-import";
 import cssnext from "postcss-cssnext";
 import BrowserSync from "browser-sync";
